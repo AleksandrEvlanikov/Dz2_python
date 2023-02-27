104d60edd5a767a54fd1128dae20903690c77bfb
Aleksandr <aleksandr.evlanicov@gmail.com>
2023-02-27 14:05:06 +0300

    Dz1

---
+++ 2.1.py
@@ -0,0 +1,9 @@
+eagle = int(input('Введите сколько орлов: '))
+tails = int(input('Введите сколько решок: '))
+
+
+if eagle < tails:
+    print('Нужно перевернуть: ',eagle )
+
+else:
+    print('Нужно перевернуть: ',tails )
